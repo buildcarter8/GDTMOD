5ded484a9d7e62f8ba5a77786711f4ad31ca4ef1
buildcarter8 <tylerhuff2002@me.com>
2013-12-28 21:18:06 -0500

    Create GTDMod.js

---
+++ Tycoon-Mod/GTDMod.js
@@ -0,0 +1,10 @@
+(function () {
+        var ready = function () {
+                
+        };
+
+        var error = function () {
+        };
+
+        GDT.loadJs(['mods/GTDMod/source/source.js'], ready, error);
+})();
